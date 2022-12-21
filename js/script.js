f422a016d0e988905a8a564e4db62cbee12e5171
chromespider <112849336+chromespider@users.noreply.github.com>
2022-12-21 18:04:23 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// Navigation buttons logic
+
+const menu = document.querySelector(".menu");
+const close = document.querySelector(".close");
+const nav = document.querySelector("nav");
+
+menu.addEventListener("click", () => {
+    nav.classList.add("open-nav");
+  });
+
+close.addEventListener("click", () => {
+    nav.classList.remove("open-nav");
+});
+
+// Animation stopper for window resize
+
+let resizeTimer;
+window.addEventListener("resize", () => {
+  document.body.classList.add("resize-animation-stopper");
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(() => {
+    document.body.classList.remove("resize-animation-stopper");
+  }, 400);
+});
